e9b29373b3b993999d7e7ab3c97a3ef72e41e73b
nacoajaree <ajaree@tsu.ac.th>
2025-04-17 10:44:20 +0700

    fill in

---
+++ listCode.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int  a;
+    int  b;
+    printf("input a and b"); scanf("%d %d",&a,&b);
+    if (a==b)
+       printf("The result a*b %d\n",a*b);
+    else  
+       if (a < b) printf("Result = a+b %d\n",a+b);
+       else 
+       printf("Result = a-b %d\n",a-b);
+    printf("Bye ");
+}
